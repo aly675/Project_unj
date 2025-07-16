330951d8e46b9423781e967f7f103ec7b994668e
yowwatoddler405 <toddleryowwa@gmail.com>
2025-07-16 11:00:37 +0700

    fix: sidebar kepala

--- resources/views/layouts/kepala-upt-layout.blade.php
+++ resources/views/layouts/kepala-upt-layout.blade.php
@@ -11,7 +11,7 @@
   <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
   </script>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&amp;display=swap" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&amp;display=swap" rel="stylesheet"/>
   <style>
    body {
       font-family: 'Poppins', sans-serif;
